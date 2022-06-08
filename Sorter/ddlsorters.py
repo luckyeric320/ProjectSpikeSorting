44761f6eb74fe500dfdf8d25f04ff4af167f2b2e
YeluSingilah <104063508+YeluSingilah@users.noreply.github.com>
2022-06-08 21:51:15 +0800

    Add files via upload

--- Sorter/ddlsorters.py
+++ Sorter/ddlsorters.py
@@ -57,3 +57,25 @@ def Sorter(raw,sorter_name):
     from ddlneurons import Neurons
     neurons = Neurons(neurons_result,time_points_s,unit_id,channel_id,Fs,raw.time_length,raw.channels_names,raw.channels_locs,raw.unit)
     return neurons
+
+def compare_sorter_results(N1,N2):
+    gamma = np.zeros([N1.n_neurons,N2.n_neurons])
+    for ne1 in range(N1.n_neurons):
+        for ne2 in range(N2.n_neurons):
+            if len([x for x in N1.channel_id[ne1] if x in N2.channel_id[ne2]])>0:
+                gamma[ne1][ne2] = get_gamma(N1.time_points[ne1],N2.time_points[ne2],0.0005) 
+    return gamma
+
+def get_gamma(trace1,trace2,tolerance):
+    coin = 0
+    p = 0
+    for tp1 in trace1:
+        for p2 in range(p,len(trace2)):
+            tp2 = trace2[p2]
+            if tp2>tp1-tolerance/2 and tp2<=tp1+tolerance/2:
+                coin += 1
+            elif tp2>tp1+tolerance/2:
+                p = p2
+                break
+    gamma = (2/(1-2*tolerance*np.max(trace1)/len(trace1)))*((coin-2*tolerance*np.max(trace1)/len(trace1)*len(trace2))/(len(trace1)+len(trace2)))
+    return gamma
