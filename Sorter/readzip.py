29aaf139bba2f56093fc41fa965a8416f7ed7301
YeluSingilah <104063508+YeluSingilah@users.noreply.github.com>
2022-06-20 22:52:42 +0800

    Add files via upload

--- Sorter/readzip.py
+++ Sorter/readzip.py
@@ -28,6 +28,7 @@ raw = raw.get_spikes()
 Neurons_list = [[] for i in range(3)]
 sorternames =  ['Klusta','Tridesclous','Spykingcircus']
 gamma = [[] for i in range(3)]
+gamma_matrix = [[[],[],[]],[[],[],[]],[[],[],[]]]
 for i in range(3):
     sortername = sorternames[i]
     Neurons_list[i] = raw.sort_by(sortername)
@@ -35,17 +36,17 @@ for i in range(3):
 #比较分类结果
 for i in range(3):
     for j in range(i+1,3):
-        gamma_matrix = compare_sorter_results(Neurons_list[i],Neurons_list[j],sorternames[i],sorternames[j]) 
-        gamma[i] = np.append(gamma[i],np.max(gamma_matrix,axis=1).reshape(gamma_matrix.shape[0],1),axis=1)
-        gamma[j] = np.append(gamma[j],np.max(gamma_matrix,axis=0).reshape(gamma_matrix.shape[1],1),axis=1)
+        gamma_matrix[i][j] = compare_sorter_results(Neurons_list[i],Neurons_list[j],sorternames[i],sorternames[j]) 
+        gamma[i] = np.append(gamma[i],np.max(gamma_matrix[i][j],axis=1).reshape(gamma_matrix[i][j].shape[0],1),axis=1)
+        gamma[j] = np.append(gamma[j],np.max(gamma_matrix[i][j],axis=0).reshape(gamma_matrix[i][j].shape[1],1),axis=1)
 for i in range(3):
     gamma[i] = np.max(gamma[i],axis=1)
 #可视化
 for i in range(3):
     #以下内容绘制到‘./str(sorternames[i])’
     for t in range(int(Neurons_list[i].time_length)):
-        Neurons_list[i].plot_neurons_locs([t,t+1],sortername,sort_by_gamma=True,gamma=gamma[i])
-    Neurons_list[i].plot_neurons_locs('all',sortername,sort_by_gamma=True,gamma=gamma[i])
+        Neurons_list[i].plot_neurons_locs([t,t+1],sorternames[i],sort_by_gamma=True,gamma=gamma[i])
+    Neurons_list[i].plot_neurons_locs('all',sorternames[i],sort_by_gamma=True,gamma=gamma[i])
     #以下内容绘制到‘./str(sorternames[i]/wave)’
     Neurons_list[i].plot_neurons_spikes('gamma',gamma=gamma[i])#每个神经元在每个电极上的波形图（多图）
 
@@ -58,7 +59,38 @@ for i in range(3):
         np.save(os.path.join('output_result/'+sorternames[i],'waveforms_'+addstr+'.npy'),Neurons_list[i].result[ne])
         np.save(os.path.join('output_result/'+sorternames[i],'timeseries_'+addstr+'.npy'),Neurons_list[i].time_points[ne])
     np.save(os.path.join('output_result/'+sorternames[i],'neurons_locs.npy'),Neurons_list[i].neurons_locs)
+#绘制分类结果对应关系图
+import networkx as nx
 
+G = nx.DiGraph()
+nodes = []
+for i in range(3):
+    nodes += Neurons_list[i].neuron_id
+for node in nodes:
+    G.add_node(node)
+for i in range(3):
+    for j in range(i+1,3):
+        for ii in range(gamma_matrix[i][j].shape[0]):
+            for jj in range(gamma_matrix[i][j].shape[1]):
+                if gamma_matrix[i][j][ii][jj]>=0.6:
+                    G.add_edge(Neurons_list[i].neuron_id[ii],Neurons_list[j].neuron_id[jj],weight=gamma_matrix[i][j][ii][jj])
+plt.rcParams['figure.figsize']=(20,20)
+labels={}
+for node in G.nodes():
+    labels[node]=node
+pos=nx.shell_layout(G)
+node_color=[]
+color=['y','b','g']
+for i in range(3):
+    for ne in range(Neurons_list[i].n_neurons):
+        node_color += [color[i]]
+nx.draw_networkx_nodes(G,pos,node_size=1000,node_color=node_color)
+nx.draw_networkx_edges(G,pos,width=[float((d['weight']-0.5)*5) for (u,v,d) in G.edges(data=True)])
+nx.draw_networkx_labels(G,pos,labels)
+plt.axis('off')
+plt.savefig('output_result/graph.png')
+plt.show()
+#生成压缩文件
 import zipfile 
 import sys 
 
@@ -73,7 +105,7 @@ def writeAllFileToZip(absDir,zipFile):
             relFile=absFile[len(os.getcwd())+1:] 
             zipFile.write(relFile)
     return
-    
+
 zipFilePath=os.path.join(sys.path[0],"output_result.zip") 
 zipFile=zipfile.ZipFile(zipFilePath,"w",zipfile.ZIP_DEFLATED) 
 absDir=os.path.join(sys.path[0],"output_result") 
